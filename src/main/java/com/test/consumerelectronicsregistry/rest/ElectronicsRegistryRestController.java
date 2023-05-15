e6b7e41acd3fe85b119a791f20774538df0e1363
Ivansh96 <evgenijshavshin@yandex.ru>
2023-05-16 02:39:35 +0700

    Registry controller added

--- src/main/java/com/test/consumerelectronicsregistry/rest/ElectronicsRegistryRestController.java
+++ src/main/java/com/test/consumerelectronicsregistry/rest/ElectronicsRegistryRestController.java
@@ -1,4 +1,33 @@
 package com.test.consumerelectronicsregistry.rest;
 
+import com.test.consumerelectronicsregistry.dal.dto.ElectronicsCreateDto;
+import com.test.consumerelectronicsregistry.dal.dto.ElectronicsReadDto;
+import com.test.consumerelectronicsregistry.dal.dto.response.ElectronicsResponse;
+import com.test.consumerelectronicsregistry.filter.ElectronicsRegistrySearchFilter;
+import com.test.consumerelectronicsregistry.service.ElectronicsRegistryFilterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.groups.Default;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/registry")
+@RequiredArgsConstructor
 public class ElectronicsRegistryRestController {
+
+    private final ElectronicsRegistryFilterService filterService;
+
+    @GetMapping("/filters")
+    public ElectronicsResponse getType(ElectronicsRegistrySearchFilter filter) {
+        return new ElectronicsResponse(new ArrayList<>(filterService.registryFilterAttributes(filter)));
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ElectronicsReadDto addEquipment(@Validated({Default.class}) @RequestBody ElectronicsCreateDto equipment) {
+        return filterService.create(equipment);
+    }
 }
